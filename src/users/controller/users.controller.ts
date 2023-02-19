8350acd1675dcab8b25b36153120592c99bb67c6
santiparra <122759810+Santiparra@users.noreply.github.com>
2023-02-19 09:40:53 -0300

    agregada la ruta con metodos para user/ users y uuid

--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { UsersService } from '../users.service';
 
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService) {}
     
-    @Get() 
-    saludoUsers (): string {    
-        return this.usersService.saludoUsers();
+    @Get("./user") 
+    getUsers(): object[] {    
+        return [{uuid:332, nombre: "asdwr", apellido:"lorem", email: "lorem@lorem"}, 
+                {uuid:333, nombre:"asdasd", apellido:"lorem", email: "lorem@lorem"}, 
+                {uuid:444, nombre:"ssrad", apellido:"lorem", email: "lorem@lorem"}];
+    }
+
+    @Get("/:uuid") 
+    getUser (@Param() params: {uuid: number, nombre: string, apellido: string, email: string}) {    
+        return params;
+    }
+
+    @Post("/user")
+    createUser(@Req() req: Request) {
+        return req.body
+    }
+
+    @Patch("/uuid")
+    actualizar(@Req() req: Request) {
+        return req.body
+    }
+
+    @Put('/:uuid')
+    update(@Param('id') params: {uuid: number, nombre: string, apellido: string, email: string}) {
+      return params;
+    }
+
+    @Delete("/:uuid") 
+    deleteUser (@Param() params: {uuid: number, nombre: string, apellido: string, email: string}) {    
+        return params;
     }
 }
+
